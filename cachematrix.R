a68a03931dce65b861f284454ae9add488914474
Nascar961 <71458882+Nascar961@users.noreply.github.com>
2023-03-04 17:19:01 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,14 @@
 ## Functions create a matrix, stores its inverse, and returns it
 
-## Write a short comment describing this function
 
+#Function creates a matrix, and also stores its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse = NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
   set_inverse <- function(inv) inverse <<- inv
   get_inverse <- function() inverse
   list(set = set, get = get,
